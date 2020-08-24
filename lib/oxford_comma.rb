4c1bbf292b0102729c6c889d1d73a073a5ade8f3
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-24 19:06:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
-    if array.length == 2
-        array.join(" and ")
-    elsif array.length > 2
-        array.insert(-2, "and")
-        array[0..-1].join(", ")
-    else
-        array.join
-    end
+  case arr.size
+when 0
+  ""
+when 1
+  arr.first
+when 2
+  arr.join(' and ')
+else
+  [arr[0..-2].join(', '), arr.last].join(', and ')
+end
 end
