0e0eae725734f3d1c84e0963abc9057a0d76e4a7
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-24 19:11:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,14 @@
 def oxford_comma(array)
-
+  case array.size
+    when 0
+      ""
+    when 1
+      arr.first
+    when 2
+      arr.join(' and ')
+    else
+      [arr[0..-2].join(', '), arr.last].join(', and ')
+    end
 
   
 end
